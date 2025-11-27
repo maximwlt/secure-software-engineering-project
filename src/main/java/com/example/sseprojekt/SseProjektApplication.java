d6ae75f80283ee90065052bc639dd71328801c91
Kaylin Althoff <kaylin.althoff@mni.thm.de>
2025-11-27 16:40:03 +0100

    Initial commit

---
+++ src/main/java/com/example/sseprojekt/SseProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.sseprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sseprojekt/SseProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sseprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
