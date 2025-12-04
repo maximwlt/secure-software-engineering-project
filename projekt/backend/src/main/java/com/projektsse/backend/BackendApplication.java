8e211bdd70573c14463bb769c49627be9585179b
mwlt39 <maxim.walter@mni.thm.de>
2025-12-04 20:19:42 +0100

    feat: adding dotenv-dependency for extracting variables from .env file into application.properties

--- projekt/backend/src/main/java/com/projektsse/backend/BackendApplication.java
+++ projekt/backend/src/main/java/com/projektsse/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.projektsse.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
@@ -11,7 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        setDotenvVariables();
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    public static void setDotenvVariables() {
+        // Load .env variables into application.properties
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 }
