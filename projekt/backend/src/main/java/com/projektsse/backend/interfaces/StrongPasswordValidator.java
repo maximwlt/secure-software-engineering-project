873b54cf3fc3743f005b6d8bfa457ad9ec3415be
mwlt39 <maxim.walter@mni.thm.de>
2026-01-14 21:09:55 +0100

    fix: adding max character password input validation

--- projekt/backend/src/main/java/com/projektsse/backend/interfaces/StrongPasswordValidator.java
+++ projekt/backend/src/main/java/com/projektsse/backend/interfaces/StrongPasswordValidator.java
@@ -2,11 +2,15 @@ package com.projektsse.backend.interfaces;
 
 import com.nulabinc.zxcvbn.Zxcvbn;
 import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 public class StrongPasswordValidator implements ConstraintValidator<StrongPassword, String> {
     @Override
-    public boolean isValid(String password, jakarta.validation.ConstraintValidatorContext context) {
+    public boolean isValid(String password, ConstraintValidatorContext context) {
         if (password == null) {
+            return false;
+        }
+        if (password.length() >= 255) {
             return false;
         }
         Zxcvbn zxcvbn = new Zxcvbn();
